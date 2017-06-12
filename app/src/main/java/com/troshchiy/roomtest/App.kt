f246bf545d803fda49200012d53bdc7550972991
yvgen.troshchiy <yvgen.troshchiy@gmail.com>
2017-06-12 11:26:19 +0300

    Add RoomDatabase in APP

--- app/src/main/java/com/troshchiy/roomtest/App.kt
+++ app/src/main/java/com/troshchiy/roomtest/App.kt
@@ -1,18 +1,18 @@
 package com.troshchiy.roomtest
 
 import android.app.Application
+import android.arch.persistence.room.Room
+import android.arch.persistence.room.RoomDatabase
 
 class App : Application() {
 
     companion object {
         lateinit var APP: App
+        lateinit var DB: RoomDatabase
     }
 
     init {
         APP = this
-    }
-
-    override fun onCreate() {
-        super.onCreate()
+        DB = Room.databaseBuilder(applicationContext, AppDatabase::class.java, "room_test_database").build()
     }
 }
