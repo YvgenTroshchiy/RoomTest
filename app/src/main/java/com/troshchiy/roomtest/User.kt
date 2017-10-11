5817b8e2911dfd54fc489b2100d6632692acd26f
YevhenTroshciy <yvgen.troshchiy@gmail.com>
2017-10-11 13:42:52 +0300

    Add age

--- app/src/main/java/com/troshchiy/roomtest/User.kt
+++ app/src/main/java/com/troshchiy/roomtest/User.kt
@@ -3,6 +3,7 @@ package com.troshchiy.roomtest
 import android.arch.persistence.room.ColumnInfo
 import android.arch.persistence.room.Entity
 import android.arch.persistence.room.PrimaryKey
+import com.troshchiy.roomtest.UserSheme.Companion.AGE
 import com.troshchiy.roomtest.UserSheme.Companion.FIRST_NAME
 import com.troshchiy.roomtest.UserSheme.Companion.LAST_NAME
 import com.troshchiy.roomtest.UserSheme.Companion.TABLE_NAME
@@ -11,7 +12,8 @@ import com.troshchiy.roomtest.UserSheme.Companion.TABLE_NAME
 @Entity(tableName = TABLE_NAME) data class User(
         @PrimaryKey(autoGenerate = true) var id: Int = 0,
         @ColumnInfo(name = FIRST_NAME) var firstName: String? = null,
-        @ColumnInfo(name = LAST_NAME) var lastName: String? = null)
+        @ColumnInfo(name = LAST_NAME) var lastName: String? = null,
+        @ColumnInfo(name = AGE) var age: Int? = null)
 //    : PaperParcelable {
 //    companion object {
 //        @JvmField val CREATOR = PaperParcelUser.CREATOR
@@ -24,5 +26,6 @@ class UserSheme {
 
         const val FIRST_NAME = "first_name"
         const val LAST_NAME = "last_name"
+        const val AGE = "age"
     }
 }
